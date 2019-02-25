1af998f1866d31800ba4cc2caf81050423bc9994
Cristian Iliescu <cristian.iliescu@avira.com>
2019-02-25 12:47:03 +0200

    Integrate redis in flow

--- src/main/scala/com/ctiliescu/scala/redis/impl/MockedRedis.scala
+++
@@ -1,7 +0,0 @@
-package com.ctiliescu.scala.redis.impl
-
-// TODO delete this
-object MockedRedis {
-  // TODO replace with redis implementation
-  val m = scala.collection.mutable.Map[String, String]()
-}
